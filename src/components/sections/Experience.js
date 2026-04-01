254414b2c87e0eb146c5a082071368c92e2aa015
codemaster0407 <chaitanyamu24@gmail.com>
2026-04-01 19:42:08 +0100

    fix: edit the role

--- src/components/sections/Experience.js
+++ src/components/sections/Experience.js
@@ -10,7 +10,7 @@ const Experience = () => {
   const experienceData = [
     {
       id: 5,
-      role: 'ML Engineer (Still to be decided)',
+      role: 'ML Engineer - Contract',
       company: 'WM Trains Ltd.',
       companyUrl: 'https://www.westmidlandsrailway.co.uk/',
       logo: `${process.env.PUBLIC_URL}/icons/wm_logo.jpeg`,
